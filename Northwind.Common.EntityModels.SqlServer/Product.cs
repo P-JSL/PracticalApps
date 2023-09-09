430b64af5c7c0b912c181f8ecb9bff3c40235bfa
P-JSL <althsuwpfl@gmail.com>
2023-09-09 11:02:54 +0900

    13장 end

--- Northwind.Common.EntityModels.SqlServer/Product.cs
+++ Northwind.Common.EntityModels.SqlServer/Product.cs
@@ -20,46 +20,25 @@ namespace Packt.Shared
 
         [Key]
         public int ProductId { get; set; }
-
-        [Required]
-        [Column(TypeName = "nvarchar (40)")]
         [StringLength(40)]
         public string ProductName { get; set; } = null!;
-
-        [Column(TypeName = "int")]
         public int? SupplierId { get; set; }
-
-        [Column(TypeName = "int")]
         public int? CategoryId { get; set; }
-
-        [Column(TypeName = "nvarchar (20)")]
         [StringLength(20)]
         public string? QuantityPerUnit { get; set; }
-
         [Column(TypeName = "money")]
         public decimal? UnitPrice { get; set; }
-
-        [Column(TypeName = "smallint")]
         public short? UnitsInStock { get; set; }
-
-        [Column(TypeName = "smallint")]
         public short? UnitsOnOrder { get; set; }
-
-        [Column(TypeName = "smallint")]
         public short? ReorderLevel { get; set; }
-
-        [Required]
-        [Column(TypeName = "bit")]
         public bool Discontinued { get; set; }
 
         [ForeignKey(nameof(CategoryId))]
         [InverseProperty("Products")]
-        public virtual Category Category { get; set; }
-
+        public virtual Category? Category { get; set; }
         [ForeignKey(nameof(SupplierId))]
         [InverseProperty("Products")]
-        public virtual Supplier Supplier { get; set; }
-
+        public virtual Supplier? Supplier { get; set; }
         [InverseProperty(nameof(OrderDetail.Product))]
         public virtual ICollection<OrderDetail> OrderDetails { get; set; }
     }
