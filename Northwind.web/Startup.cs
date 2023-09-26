d92e42ed5fde21bdb910866f7ebb146b7ffc94d4
P-JSL <althsuwpfl@gmail.com>
2023-09-26 22:04:07 +0900

    14장 끝 15장 시작

--- Northwind.web/Startup.cs
+++ Northwind.web/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Packt.Shared;
+﻿using static System.Console;
+using Packt.Shared;
 
 namespace Northwind.web;
 
@@ -18,6 +19,26 @@ public class Startup
             app.UseHsts();
         }
         app.UseRouting();
+        app.Use(async (HttpContext context, Func<Task> next) =>
+        {
+            RouteEndpoint? rep = context.GetEndpoint() as RouteEndpoint;
+            if (rep is not null)
+            {
+                WriteLine($"Endpoing name: {rep.DisplayName}");
+                WriteLine($"Endpoing route pattern : {rep.RoutePattern.RawText}");
+            }
+            else
+            {
+                if(context.Request.Path == "/bonjour")
+                {
+                    //url 경로가 일치하면 응답을 반환하는 종료 대리자가 되므로
+                    //다음 대리자를 호출하지 않음
+                    await context.Response.WriteAsync("Bonjour Monde!");
+                    return;
+                }
+            }
+            await next();
+        });
         app.UseHttpsRedirection();
         app.UseDefaultFiles();
         app.UseStaticFiles();
